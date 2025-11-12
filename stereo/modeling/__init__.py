8f82b0a465bdc1e8fcb9ff02d4bd47877fa56619
AniruthSuresh <aniruthzlatan@gmail.com>
2025-11-13 03:30:07 +0530

    adding dynamic output folder name change

--- stereo/modeling/__init__.py
+++ stereo/modeling/__init__.py
@@ -44,7 +44,7 @@ __all__ = {
     'CasPSMNet': CasStereoTrainer,
     'LightStereo': LightStereoTrainer,
     'StereoBaseGRU': StereoBaseGRUTrainer,
-    'FoundationStereo': FoundationStereoTrainer,
+    # 'FoundationStereo': FoundationStereoTrainer,
     # 'IInet': IINetTrainer,
     # 'NMRF': NMRFTrainer
 }
